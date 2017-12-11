0fd4c36dc91b0803b056f9b7c7c59be0d0b75631
Govinda <vegarana44@gmail.com>
2017-12-11 12:45:53 +0545

    second update

---
+++ include/head.php
@@ -0,0 +1,26 @@
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width ,initial-scale=1.0">
+	<link rel="shortcut icon" type="image/jpg" href="images/logo.png">
+	<!--style css -->
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!-- fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.css">
+	<!-- carousel -->
+	<link rel="stylesheet" href="owl/owl.carousel.min.css">
+	<link rel="stylesheet" href="owl/owl.theme.default.min.css">
+	<!-- responsive -->
+	<link rel="stylesheet" media="only screen and (max-width: 960px)" href="css/max-960px.css"/>
+	<link rel="stylesheet" media="only screen and (max-width: 500px )" href="css/max-500px.css"/>
+	<!-- facebook -->
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.11';
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+	<!-- jquery -->
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
---
+++ include/header.php
@@ -0,0 +1,57 @@
+<header>
+		<div class="center clearfix">
+			<div class="btn">
+				</div>
+				<div class="logose">
+					<img src="images/logo.png" alt="LOGO">
+				</div>
+				<a href="#" class="vol">become volunteer</a>
+			<div class="logos">
+				<ul class="social">		
+					<li>
+						<a href="https://www.instagram.com">
+						<i  class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<a href="https://twitter.com">
+						<i  class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<a href="https://www.youtube.com">
+						<i  class="fa fa-youtube-play fa-2x" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<a href="https://np.linkedin.com">
+							<i class="fa fa-linkedin fa-2x" aria-hidden="true"></i>
+						</a>
+					</li>
+				</ul>
+				<a class="fa fa-angle-right" aria-hidden="true"></a>
+				
+					<ul class="menu">
+						<li>
+							<a href="#">Home</a>
+								</li>
+								<li>
+									<a href="#">About</a>
+								</li>
+								<li>
+									<a href="#">Our team</a>
+								</li>
+								<li>
+									<a href="#">our project</a>
+								</li>
+								<li>
+									<a href="#">partners</a>
+								</li>
+								<li>
+									<a href="#">achievements</a>
+								</li>
+								<li>
+							<a href="#">contact us</a>
+						</li>
+					</ul>
+				</div>
+					<input type="text">
+			</div>
+	</header>
